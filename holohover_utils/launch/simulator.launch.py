1780c3464bb64409e3b1ed7f4c432c6f8a8c6f39
Roland Schwan <roland.schwan@mikrounix.com>
2022-01-05 15:55:13 +0100

    restructured project

---
+++ holohover_utils/launch/simulator.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    simulator_node = Node(
+        package="holohover_gnc",
+        executable="simulator",
+    )
+
+    controller_node = Node(
+        package="holohover_gnc",
+        executable="controller"
+    )
+
+    estimator_node = Node(
+        package="holohover_gnc",
+        executable="estimator"
+    )
+
+    ld.add_action(simulator_node)
+    ld.add_action(estimator_node)
+    # ld.add_action(controller_node)
+    return ld
