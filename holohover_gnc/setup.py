65884812ba7af72af1b41f0243aef7e95e99cb96
Roland Schwan <roland.schwan@mikrounix.com>
2022-01-08 01:47:39 +0100

    remove duplicate launch file

--- holohover_gnc/setup.py
+++ holohover_gnc/setup.py
@@ -11,7 +11,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     package_data={
         '': ['*.csv'],
