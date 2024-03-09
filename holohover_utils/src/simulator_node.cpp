71647ab2537c514d4465d6f72d1a4b2acd2e8c45
Andrea Grillo <andrea.grillo@epfl.ch>
2024-03-09 13:40:59 +0100

    simulator node working but poses diverge

--- holohover_utils/src/simulator_node.cpp
+++ holohover_utils/src/simulator_node.cpp
@@ -4,13 +4,26 @@
 
 
 SimulatorNode::SimulatorNode() :
-        Node("simulator"),
-        gravity(0.0f, 0.0f),
-        world(gravity)
+    Node("simulator"),
+    holohover_props(load_holohover_pros(*this)),
+    simulation_settings(load_simulation_settings(*this)),
+    holohover(holohover_props, simulation_settings.period),
+    gravity(0.0f, 0.0f),
+    world(gravity)
 {
+    hovercraft_ids = declare_parameter<std::vector<long int>>("hovercrafts");
+    hovercraft_shape.m_radius = 1.0f;        // ToDo parameter
+    // ToDo density parameter
+
+    timeStep = 1.0f / 60.0f;              // simulating at 60Hz - ToDo parametrize
+
+    // number of internal iterations
+    velocityIterations = 6;               // ToDo parametrize
+    positionIterations = 2;
+
+
     init_box2d_world();
-    init_box2d_hovercrafts();
-    init_topics();
+    init_hovercrafts();
     init_timer();
 }
 
@@ -25,75 +38,68 @@ void SimulatorNode::init_box2d_world()
     groundBody->CreateFixture(&groundBox, 0.0f);                    // 0 density for static body
 }
 
-void SimulatorNode::init_box2d_hovercrafts()
+void SimulatorNode::init_hovercrafts()
 {
-    // hovercraft shape
-    b2CircleShape hoverCraftShape;
-    hoverCraftShape.m_radius = 1.0f;        // ToDo
 
-    // ToDo if hovercraft enabled in params
+    for(long int hovercraft_id : hovercraft_ids) {
+        RCLCPP_INFO(get_logger(), "Hovercraft id: %ld", hovercraft_id);
+        
+        //box2d bodies
+        b2BodyDef bodyDef;
+        bodyDef.type = b2_dynamicBody;
+        bodyDef.position.Set(0.0f, 0.0f);               // ToDo get starting positions
+        bodyDef.angle = 0.0f;
+        bodyDef.linearVelocity = b2Vec2(0.0f, 0.0f);
+        bodyDef.angularVelocity = 0.0f;
+        b2Body * body = world.CreateBody(&bodyDef);
+        body->CreateFixture(&hovercraft_shape, 1.0f);               // ToDo non-zero density for dynamic body
+        hovercraft_bodies.push_back(body);
 
-    // hovercraft 0
-    b2BodyDef hovercraft0_bodyDef;
-    hovercraft0_bodyDef.type = b2_dynamicBody;
-    hovercraft0_bodyDef.position.Set(0.0f, 4.5f);               // ToDo get starting positions
-    hovercraft0 = world.CreateBody(&hovercraft0_bodyDef);
-    hovercraft0->CreateFixture(&hoverCraftShape, 1.0f);               // ToDo non-zero density for dynamic body
+        // state
+        Holohover::state_t<double> state;
+        body_to_state(state, body);
+        states_vec.push_back(state);
 
-    // hovercraft 1
-    b2BodyDef hovercraft1_bodyDef;
-    hovercraft1_bodyDef.type = b2_dynamicBody;
-    hovercraft1_bodyDef.position.Set(0.0f, 4.5f);               // ToDo get starting positions
-    hovercraft1 = world.CreateBody(&hovercraft1_bodyDef);
-    hovercraft1->CreateFixture(&hoverCraftShape, 1.0f);               // ToDo non-zero density for dynamic body
+        // motor velociticies
+        Holohover::control_force_t<double> motor_velocities;
+        motor_velocities.setZero();
+        motor_velocities_vec.push_back(motor_velocities);
+
+        // control acc
+        Holohover::control_acc_t<double> current_control_acc;
+        calculate_control_acc(state, motor_velocities, current_control_acc);
+        control_acc_vec.push_back(current_control_acc);
+
+        // control messages
+        holohover_msgs::msg::HolohoverControlStamped msg;
+        // init zero control
+        msg.header.frame_id = "body";
+        msg.header.stamp = this->now();
+        msg.motor_a_1 = 0;
+        msg.motor_a_2 = 0;
+        msg.motor_b_1 = 0;
+        msg.motor_b_2 = 0;
+        msg.motor_c_1 = 0;
+        msg.motor_c_2 = 0;
+        control_msgs.push_back(msg);
+
+
+        // control subscriptions
+        auto topic_name = "/hovercraft" + std::to_string(hovercraft_id) + "/control";
+
+        std::function<void(const holohover_msgs::msg::HolohoverControlStamped::SharedPtr)> callback = 
+                 std::bind(&SimulatorNode::control_callback, this, std::placeholders::_1, hovercraft_id);
+
+        auto sub = this->create_subscription<holohover_msgs::msg::HolohoverControlStamped>(topic_name, rclcpp::SensorDataQoS(), callback);
+
+        control_subscriptions.push_back(sub);
+
+        // pose publishers
+        topic_name = "/hovercraft" + std::to_string(hovercraft_id) + "/pose";
+        pose_publishers.push_back(this->create_publisher<geometry_msgs::msg::PoseStamped>(
+            topic_name, rclcpp::SensorDataQoS()));
+    }
     
-    // hovercraft 2
-    b2BodyDef hovercraft2_bodyDef;
-    hovercraft2_bodyDef.type = b2_dynamicBody;
-    hovercraft2_bodyDef.position.Set(0.0f, 4.5f);               // ToDo get starting positions
-    hovercraft2 = world.CreateBody(&hovercraft2_bodyDef);
-    hovercraft2->CreateFixture(&hoverCraftShape, 1.0f);               // ToDo non-zero density for dynamic body
-
-    // hovercraft 3
-    b2BodyDef hovercraft3_bodyDef;
-    hovercraft3_bodyDef.type = b2_dynamicBody;
-    hovercraft3_bodyDef.position.Set(0.0f, 4.5f);               // ToDo get starting positions
-    hovercraft3 = world.CreateBody(&hovercraft3_bodyDef);
-    hovercraft3->CreateFixture(&hoverCraftShape, 1.0f);               // ToDo non-zero density for dynamic body
-}
-
-void SimulatorNode::init_topics()
-{   
-    // Todo parameters check if each hovercraft is enabled
-
-    control_subscription_0 = this->create_subscription<holohover_msgs::msg::HolohoverControlStamped>(
-            "hovercraft0/control", rclcpp::SensorDataQoS(),
-            std::bind(&SimulatorNode::control_callback_0, this, std::placeholders::_1));
-
-    control_subscription_1 = this->create_subscription<holohover_msgs::msg::HolohoverControlStamped>(
-            "hovercraft1/control", rclcpp::SensorDataQoS(),
-            std::bind(&SimulatorNode::control_callback_1, this, std::placeholders::_1));
-
-    control_subscription_2 = this->create_subscription<holohover_msgs::msg::HolohoverControlStamped>(
-            "hovercraft2/control", rclcpp::SensorDataQoS(),
-            std::bind(&SimulatorNode::control_callback_2, this, std::placeholders::_1));
-
-    control_subscription_3 = this->create_subscription<holohover_msgs::msg::HolohoverControlStamped>(
-            "hovercraft3/control", rclcpp::SensorDataQoS(),
-            std::bind(&SimulatorNode::control_callback_3, this, std::placeholders::_1));
-    
-    pose_publisher_0 = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-            "hovercraft0/pose", rclcpp::SensorDataQoS());
-
-    pose_publisher_1 = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-            "hovercraft1/pose", rclcpp::SensorDataQoS());
-
-    pose_publisher_2 = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-            "hovercraft2/pose", rclcpp::SensorDataQoS());
-
-    pose_publisher_3 = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-            "hovercraft3/pose", rclcpp::SensorDataQoS());
-
 }
 
 void SimulatorNode::init_timer()
@@ -105,100 +111,84 @@ void SimulatorNode::init_timer()
 
 void SimulatorNode::simulation_step()
 {
-    // - control_* to force/torque to apply for each hovercraft - ToDo copy from old simulation node
-    // ....
-
-    // - apply forces to box2d bodies
-    // hovercraft0->ApplyForceToCenter();
-    // hovercraft0->ApplyTorque(0.0f);
-
-    // hovercraft1->ApplyForceToCenter();
-    // hovercraft0->ApplyTorque(0.0f);
-
-    // hovercraft2->ApplyForceToCenter();
-    // hovercraft0->ApplyTorque(0.0f);
-
-    // hovercraft3->ApplyForceToCenter();
-    // hovercraft0->ApplyTorque(0.0f);
-
+    for(long int i : hovercraft_ids) {
+        // - control_* to force/torque to apply for each hovercraft - ToDo copy from old simulation node
+        // integrate motor velocities
+        Holohover::control_force_t<double> current_control_signal;
+        current_control_signal(0) = control_msgs[i].motor_a_1;
+        current_control_signal(1) = control_msgs[i].motor_a_2;
+        current_control_signal(2) = control_msgs[i].motor_b_1;
+        current_control_signal(3) = control_msgs[i].motor_b_2;
+        current_control_signal(4) = control_msgs[i].motor_c_1;
+        current_control_signal(5) = control_msgs[i].motor_c_2;
+        motor_velocities_vec[i] = holohover.Ad_motor * motor_velocities_vec[i] + holohover.Bd_motor * current_control_signal;
+        calculate_control_acc(states_vec[i], motor_velocities_vec[i], control_acc_vec[i]);
+        apply_control_acc(hovercraft_bodies[i], control_acc_vec[i]);
+        
+    }
 
     // - box2d step of the world
-
-    float timeStep = 1.0f / 60.0f;              // simulating at 60Hz - ToDo parametrize
-
-    // number of internal iterations
-    int32 velocityIterations = 6;               // ToDo parametrize
-    int32 positionIterations = 2;
     world.Step(timeStep, velocityIterations, positionIterations);
 
-    // - get position and orientation of each hovercraft from box2d
-    b2Vec2 position0 = hovercraft0->GetPosition();
-    float angle0 = hovercraft0->GetAngle();
-
-    b2Vec2 position1 = hovercraft1->GetPosition();
-    float angle1 = hovercraft1->GetAngle();
-
-    b2Vec2 position2 = hovercraft2->GetPosition();
-    float angle2 = hovercraft2->GetAngle();
-
-    b2Vec2 position3 = hovercraft3->GetPosition();
-    float angle3 = hovercraft3->GetAngle();
-
-    // - publish pose of each hovercraft
-    // pose_publisher_0->publish(..);
-
-    /* from old simulation node - ToDo
-    
-    geometry_msgs::msg::PoseStamped pose_measurement;
-    pose_measurement.header.frame_id = "world";
-    pose_measurement.header.stamp = this->now();
-    pose_measurement.pose.position.x = state(0) + simulation_settings.Gx;
-    pose_measurement.pose.position.y = state(1) + simulation_settings.Gy;
-    pose_measurement.pose.position.z = 0;
-    double theta = state(4);
-
-    std::normal_distribution<> pose_x_noise(0, simulation_settings.sensor_pose_noise_x);
-    std::normal_distribution<> pose_y_noise(0, simulation_settings.sensor_pose_noise_y);
-    std::normal_distribution<> pose_yaw_noise(0, simulation_settings.sensor_pose_noise_yaw);
-    pose_measurement.pose.position.x += pose_x_noise(random_engine);
-    pose_measurement.pose.position.y += pose_y_noise(random_engine);
-    theta += pose_yaw_noise(random_engine);
-
-    if (theta < -M_PI)
-    {
-        theta += 2 * M_PI;
+    for(long int i : hovercraft_ids) {
+        body_to_state(states_vec[i], hovercraft_bodies[i]);
+       
+        geometry_msgs::msg::PoseStamped pose_measurement;
+        pose_measurement.header.frame_id = "world";
+        pose_measurement.header.stamp = this->now();
+        pose_measurement.pose.position.x = states_vec[i](0);// ToDo??? + simulation_settings.Gx;
+        pose_measurement.pose.position.y = states_vec[i](1);// + simulation_settings.Gy;
+        pose_measurement.pose.position.z = 0;
+        
+        tf2::Quaternion q;
+        q.setRPY(0, 0, states_vec[i](4));
+        pose_measurement.pose.orientation.w = q.w();
+        pose_measurement.pose.orientation.x = q.x();
+        pose_measurement.pose.orientation.y = q.y();
+        pose_measurement.pose.orientation.z = q.z();
+        
+        pose_publishers[i]->publish(pose_measurement);
     }
-    if (theta > M_PI)
-    {
-        theta -= 2 * M_PI;
-    }
-
-    tf2::Quaternion q;
-    q.setRPY(0, 0, theta);
-    pose_measurement.pose.orientation.w = q.w();
-    pose_measurement.pose.orientation.x = q.x();
-    pose_measurement.pose.orientation.y = q.y();
-    pose_measurement.pose.orientation.z = q.z();
-
-    pose_publisher->publish(pose_measurement);*/
 }
 
-void SimulatorNode::control_callback_0(const holohover_msgs::msg::HolohoverControlStamped &msg) {
-    control_0 = msg;
+void SimulatorNode::control_callback(holohover_msgs::msg::HolohoverControlStamped::SharedPtr msg, long int hovercraft_id) {
+    control_msgs[hovercraft_id] = *msg;
 }
 
-void SimulatorNode::control_callback_1(const holohover_msgs::msg::HolohoverControlStamped &msg) {
-    control_1 = msg;
+void SimulatorNode::body_to_state(Holohover::state_t<double> &state, b2Body* body) {
+    // ToDo add noise 
+    state(0) = body->GetPosition().x;
+    state(1) = body->GetPosition().y;
+    state(2) = body->GetLinearVelocity().x;
+    state(3) = body->GetLinearVelocity().y;
+    state(4) = body->GetAngle();
+    state(5) = body->GetAngularVelocity();
 }
 
-void SimulatorNode::control_callback_2(const holohover_msgs::msg::HolohoverControlStamped &msg) {
-    control_2 = msg;
+void SimulatorNode::apply_control_acc(b2Body* body, Holohover::control_acc_t<double> control_acc) {
+    body->ApplyForceToCenter(b2Vec2(control_acc(0) * holohover_props.mass, control_acc(1) * holohover_props.mass), true);
+    body->ApplyTorque(control_acc(2) * holohover_props.inertia, true);
 }
 
-void SimulatorNode::control_callback_3(const holohover_msgs::msg::HolohoverControlStamped &msg) {
-    control_3 = msg;
-}
+void SimulatorNode::calculate_control_acc(Holohover::state_t<double> state, Holohover::control_force_t<double> motor_velocities, Holohover::control_acc_t<double> &current_control_acc)
+{
+    Holohover::control_force_t<double> current_control_force;
+    holohover.signal_to_thrust(motor_velocities, current_control_force);
+    holohover.control_force_to_acceleration(state, current_control_force, current_control_acc);
 
+    // add drag force
+    double rho = 1.2; // air density at ~20C sea level
+    current_control_acc(0) -= 0.5 / holohover_props.mass * rho * state(2) * abs(state(2)) * simulation_settings.drag_coefficient * simulation_settings.drag_reference_area;
+    current_control_acc(1) -= 0.5 / holohover_props.mass * rho * state(3) * abs(state(3)) * simulation_settings.drag_coefficient * simulation_settings.drag_reference_area;
+
+    // add force introduced by table tilt
+    double g = 9.81;
+    current_control_acc(0) += sin(simulation_settings.table_tilt_x * M_PI / 180.0) * g;
+    current_control_acc(1) += sin(simulation_settings.table_tilt_y * M_PI / 180.0) * g;
+
+    //std::cout << "acc = " << current_control_acc.transpose() << std::endl;
+    //std::cout << "force = " << current_control_force.transpose() << std::endl;
+}
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
