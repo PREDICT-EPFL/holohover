cd3abb30b9c3aa5ee485492e31a433d35e5d4116
Goesta Stomberg <goesta.stomberg@tu-dortmund.de>
2024-06-17 15:35:15 +0200

    updated clover trajectories to finish in the center of the table

--- holohover_dmpc/ocp_specs/matlab/gen_clover_traj.m
+++ holohover_dmpc/ocp_specs/matlab/gen_clover_traj.m
@@ -39,15 +39,36 @@ function write_clover_traj_to_csv(N,xref,uref,dt,t0)
     t(1) = 0;
     x(1,:) = [repmat(x1d.',1,N+1),x2d.'];
     u(1,:) = zeros(1,3*N);
+
+    %load clover into horizon
+    for k = 1:N+1
+        t(end+1) = ceil(t0/dt) + (k-1);
+        xmat = xref(:,1:1+k-1);
+        umat = uref(:,1:k-1);
+        x(end+1,:) = [repmat(x1d.',1,N+1-k),vertcat(xmat(:)).',x2d.'];
+        u(end+1,:) = [zeros(1,3*(N-k+1)),vertcat(umat(:)).'];
+    end
     
-    %clover
+    %traverse clover
     for k = 1:time_steps
-        t(k+1) = ceil(t0/dt) + (k-1);
+        t(end+1) = t(end)+1;
         xmat = xref(:,k:k+N);
         umat = uref(:,k:k+N-1);
-        x(k+1,:) = [vertcat(xmat(:)).',x2d.'];
-        u(k+1,:) = vertcat(umat(:)).';
+        x(end+1,:) = [vertcat(xmat(:)).',x2d.'];
+        u(end+1,:) = vertcat(umat(:)).';
     end
+
+    %return and stay in starting position
+    for k = 1:N
+        t(end+1) = t(end)+1;
+        xmat = xref(:,end-N+k:end);
+        umat = uref(:,end-N+k+1:end);
+        x(end+1,:) = [vertcat(xmat(:)).',repmat(x1d.',1,k),x2d.'];
+        u(end+1,:) = [vertcat(umat(:)).',zeros(1,3*(k))];
+    end
+    t(end+1) = t(end)+1;
+    x(end+1,:) = [repmat(x1d.',1,N+1),x2d.'];
+    u(end+1,:) = zeros(1,3*N);
     
     X = [t.',x];
     U = [t.',u];
