22199509413f62f460ae01446f9432101b89f10c
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-19 08:39:28 +0200

    launch file: parsing of experiment file to have right namespace for the nodes

--- holohover_utils/launch/common/embedded_controller.launch.py
+++ holohover_utils/launch/common/embedded_controller.launch.py
@@ -2,10 +2,39 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+import yaml
 
+def launch_setup(context):
+    experiment_filename = LaunchConfiguration('experiment').perform(context)
+    machine = LaunchConfiguration('machine').perform(context)
+    name = ''
+    launch_description = []
 
-def generate_launch_description():
-    ld = LaunchDescription()
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    experiment_conf = os.path.join(
+        get_package_share_directory('holohover_utils'), 
+        'config', 
+        'experiments', 
+        experiment_filename
+    )
+
+    data = yaml.safe_load(open(experiment_conf, 'r'))
+    hovercraft = data["hovercraft"]
+     
+    for h in hovercraft:
+        if h['machine'] == machine:
+            name = h['name']
+            break
+    
+    if name == '':
+        print("Not starting any controller")
+        return launch_description
+
+    print("Starting controller for hovercraft", name)
+
 
     drivers_config = os.path.join(
         get_package_share_directory('holohover_utils'),
@@ -18,6 +47,7 @@ def generate_launch_description():
         name="holohover_fc",
         package="holohover_drivers",
         executable="holohover_fc",
+        namespace=name,
         parameters=[drivers_config],
         output='both'
     )
@@ -26,11 +56,34 @@ def generate_launch_description():
         name="holohover_mouse_sensor",
         package="holohover_drivers",
         executable="holohover_mouse_sensor",
+        namespace=name,
         parameters=[drivers_config],
         output='both'
     )
 
-    ld.add_action(fc_node)
-    #ld.add_action(mouse_node)
+    launch_description.append(fc_node)
+    #launch_description.append(mouse_node)
 
+    return launch_description
+
+
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    opfunc = OpaqueFunction(function = launch_setup)
+
+    ld.add_action(DeclareLaunchArgument(
+        'experiment', default_value='experiment1.yaml',
+        description='Experiment File'
+    ))
+
+    ld.add_action(DeclareLaunchArgument(
+        'machine', default_value='master',
+        description='Machine Name'
+    ))
+
+    ld.add_action(opfunc)
+ 
     return ld
