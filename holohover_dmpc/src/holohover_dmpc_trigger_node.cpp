4ac37d3ab627cf0e73e258d78a09b9e80e829c04
Andrea Grillo <andrea.grillo@epfl.ch>
2024-06-24 18:27:10 +0200

    cleaned up trigger node

--- holohover_dmpc/src/holohover_dmpc_trigger_node.cpp
+++ holohover_dmpc/src/holohover_dmpc_trigger_node.cpp
@@ -5,38 +5,21 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int64.hpp"
-#include "control_dmpc_settings.hpp"
 
 using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
 
 class HolohoverDmpcTrigger : public rclcpp::Node
 {
   public:
-    HolohoverDmpcTrigger()
-    : Node("holohover_dmpc_trigger"), control_settings(load_control_dmpc_settings(*this)), count_(0)
+    HolohoverDmpcTrigger() : Node("holohover_dmpc_trigger")
     {
-      publisher_ = this->create_publisher<std_msgs::msg::UInt64>("/dmpc/trigger", 10);
+      auto publisher_ = this->create_publisher<std_msgs::msg::UInt64>("/dmpc/trigger", 10);
       std::this_thread::sleep_for(std::chrono::seconds(15));
       RCLCPP_INFO(get_logger(), " - - - STARTING TRIGGERING NOW - - - ");
-      timer_ = this->create_wall_timer(
-      std::chrono::duration<double>(control_settings.dmpc_period), std::bind(&HolohoverDmpcTrigger::timer_callback, this));
-    }
-
-  private:
-    void timer_callback()
-    {
       auto message = std_msgs::msg::UInt64();
-      count_++;
-      message.data = count_;
+      message.data = 0;
       publisher_->publish(message);
     }
-    ControlDMPCSettings control_settings;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
-    size_t count_;
 };
 
 int main(int argc, char * argv[])
