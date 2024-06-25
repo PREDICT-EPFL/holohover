290d3e097ea7931fb2be849d8dae68922ee5818e
Goesta Stomberg <goesta.stomberg@tu-dortmund.de>
2024-06-25 09:33:30 +0200

    added OCP with stronger weights on position

--- holohover_dmpc/ocp_specs/matlab/gen_holohover_OCP_acc.m
+++ holohover_dmpc/ocp_specs/matlab/gen_holohover_OCP_acc.m
@@ -24,9 +24,9 @@ import casadi.*
 
 % scenario
 Nrobot = 4;
-N = 10;          %horizon
-dt = 0.100;       %sampling interval seconds
-h = 0.100; %OCP shooting interval
+N = 20;          %horizon
+dt = 0.050;       %sampling interval seconds
+h = 0.050; %OCP shooting interval
 xx0 = {[0.5;0;zeros(4,1)],[0.15;0.3;zeros(4,1)],[-0.15;-0.3;zeros(4,1)],[-0.5;0;zeros(4,1)]};
 uu0 = {[0;0;0],[0;0;0],[0;0;0],[0;0;0]};
 
@@ -46,7 +46,7 @@ xinit = []; %solver initialization
 % setup OCP
 %sProb = holohover_sProb_acc(Nrobot,N,dt,h,xx0,uu0,xxd,xinit);
 %sProb = holohover_sProb_QP2(Nrobot,N,dt,h,xx0,uu0,xxd,xinit);
-sProb = holohover_sProb_QP3(Nrobot,N,dt,h,xx0,uu0,xxd,xinit);
+sProb = holohover_sProb_QP4(Nrobot,N,dt,h,xx0,uu0,xxd,xinit);
 
 %export C code
 gen_c_sProb(sProb);
