ec8fdaf2a1c94fa51282165b05d631ab20656675
Goesta Stomberg <goesta.stomberg@tu-dortmund.de>
2024-06-27 13:29:28 +0200

    added line crossing with static and dynamic obstacle scenario

--- holohover_utils/launch/hovercraft_lqr.launch.py
+++ holohover_utils/launch/hovercraft_lqr.launch.py
@@ -35,14 +35,14 @@ def launch_setup(context):
     )
 
     # - - - Nodes
-    # navigation_node = Node(
-    #     package="holohover_navigation",
-    #     executable="navigation_disturbance",
-    #     parameters=[navigation_config, {'holohover_props_file' : params}],
-    #     namespace= name,
-    #     output='screen',
-    #     prefix='nice -n -19'
-    # )
+    navigation_node = Node(
+        package="holohover_navigation",
+        executable="navigation_disturbance",
+        parameters=[navigation_config, {'holohover_props_file' : params}],
+        namespace= name,
+        output='screen',
+        prefix='nice -n -19'
+    )
     
     controller_node = Node(
         name="lqr",
@@ -64,8 +64,12 @@ def launch_setup(context):
             )
 
     launch_description.append(controller_node)        
+<<<<<<< HEAD
     # launch_description.append(navigation_node)
     launch_description.append(recorder_launch)
+=======
+    launch_description.append(navigation_node)
+>>>>>>> 8db2546 (added line crossing with static and dynamic obstacle scenario)
 
     return launch_description
 
