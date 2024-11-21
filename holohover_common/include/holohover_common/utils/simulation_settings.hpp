fd28aeef180ecaef35789c212796afccf493bbb9
Andrea Grillo <andrea.grillo@epfl.ch>
2024-11-21 01:50:10 +0100

    removed default value for are_all_simulated in simulation_settings to avoid errors

--- holohover_common/include/holohover_common/utils/simulation_settings.hpp
+++ holohover_common/include/holohover_common/utils/simulation_settings.hpp
@@ -64,7 +64,7 @@ SimulationSettings load_simulation_settings(rclcpp::Node &node)
     settings.internal_iterations_velocity = node.declare_parameter<int>("internal_iterations_velocity");
     settings.internal_iterations_position = node.declare_parameter<int>("internal_iterations_position");
 
-    settings.are_all_simulated    = node.declare_parameter<bool>("are_all_simulated", false);
+    settings.are_all_simulated    = node.declare_parameter<bool>("are_all_simulated");
     settings.hovercraft_ids       = node.declare_parameter<std::vector<long int>>("hovercraft_ids");
     settings.hovercraft_names     = node.declare_parameter<std::vector<std::string>>("hovercraft_names");
     settings.holohover_props_files= node.declare_parameter<std::vector<std::string>>("holohover_props_files");
